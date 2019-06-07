88bea856e3e024aa5c0ed576f059a93d95f773c6
jaakofalltrade <jaako.andes@bicol-u.edu.ph>
2019-06-07 14:06:11 +0800

    added boxes, almost finished with the initial design

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    /*let interVal = true;
+    setInterval(function() {
+        if(interVal == true) {
+            $('title').html('Welcome to PokeBoom!');   
+        } else {
+            $('title').html('Enjoy your visit!');
+        }
+        interVal = !interVal;
+    }, 5000);*/
+});
